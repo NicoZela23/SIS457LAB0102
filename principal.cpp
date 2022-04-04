8cf2942eb9ff65fb9d7644e910dc9609226cea6e
Nicolas Zelaya <fezelayani@gmail.com>
2022-04-04 15:54:57 -0400

    Creacion del repo en Github

---
+++ principal.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int dec;
+    dec = 1;
+    while (dec == 1)
+    {
+    cout<<"Mi nombre es Nicolas Zelaya Orellana, tengo 20 años, soy estudiante de la carrera de Ingeniería en Ciencias de la Computación en la universidad San Francisco Xavier de Chuquisaca, me considero un aficionado en la tecnología, música, fotografía, diseño y videojuegos."<<endl;
+    cout<<""<<endl;
+    cout<<"En cuanto al ámbito profesional, en el pasado (épocas de colegio) estuve muy interesado en la robótica pudiendo ganar las olimpiadas el último año y quedar en cuarto puesto con mi equipo en la competencia de first global, además de haber pasado un año por la carrera de ingeniería industrial intentando seguir los pasos y consejos de familiares pero claramente no me gusto ni me sentí cómodo con el futuro que iba a tener así que escape a un lugar donde siempre me gusto estar, frente a una computadora. Actualmente tengo como objetivo ser un desarrollador web full stack y diseñador en el tiempo libre, pude trabajar brevemente en el lado del frontend con páginas bastante básica pero que ayudaron para mi aprendizaje, dedico cierto tiempo en aprender sobre distintos lenguajes, frameworks y tecnologías en plataformas como Platzi y Udemy y el infaltable Youtube, además pude aprender sobre Photoshop y manejos básicos de color donde puedo realizar ciertos artes para comunidades a las que pertenezco (Mujers-tics, GDG, MUG) y aprovechando el tiempo libre en hacer imágenes editadas como hobby y expresión además de “intentar” conseguir buenas fotos de paisajes y sus respectivas ediciones."<<endl;
+    cout<<""<<endl;
+    cout<<"Del ámbito musical, toco la armónica, kena y canto (en bandas de bar o en el coro de la universidad) además de estar todo el tiempo escuchando música, ya sea para relajarme o como un pasatiempo. Normalmente escucho música de los géneros Rock y hip hop y trato de explorar en todos los géneros posibles pudiendo escuchar desde Iron Maiden hasta Maroyu."<<endl;
+    cout<<""<<endl;
+    cout<<"En cuanto a los videojuegos heredé un gusto por los clásicos como Quake, Doom, Mario Bros, Hexen, Contra, Galaga, Zelda, etc. Mismos que lograron definir mis gustos de infancia hacia lo que consumo ahora. Juego regularmente ya sea solo o con amigos, disfruto mucho las sagas Dark Souls, Fallout y borderlands, Counter strike, WOW o Terraria pero esto es solo con amigos."<<endl;
+    cout<<""<<endl;
+    cout<<"En conclusión, cual estudiante más de tecnología busco estar cuanto más tiempo pueda frente a un monitor, Me encuentro en la etapa adolescente/adulto donde aun puedo experimentar y definir mi camino a seguir en el futuro profesional, sin olvidarme de los pasatiempos y pasiones que creo que es lo que mantiene mis pies sobre la tierra."<<endl;
+    cout<<""<<endl;
+    cout<<"¿Desea imprimir la biografía nuevamente?"<<endl;
+    cout<<""<<endl;
+    cout<<"Si: 1"<<endl;
+    cout<<"No: 0"<<endl;
+
+        cin >> dec;
+    }
+    return 0;
+}
